bd7d41e89450b1b8d1b831ad696d34a671035025
Kabayos <redstillness@gmail.com>
2025-09-26 20:36:15 +0800

    login form

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+if (isset($_SESSION['user_id'])) {
+    header("Location: admin_dashboard.php");
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <form method="POST" action="login_process.php">
+        <label>Username:</label>
+        <input type="text" name="username" required><br><br>
+
+        <label>Password:</label>
+        <input type="password" name="password" required><br><br>
+
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
