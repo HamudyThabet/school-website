5c9e302c514b2f27b3ff439192e5e52d18e34188
RyotaDelaCruz <ryotadelacruz@gmail.com>
2025-10-03 14:52:55 +0800

    Update index.php

--- User Interface/index.php
+++ User Interface/index.php
@@ -1,6 +1,6 @@
-<?php include "header.php"; ?>
+<?php include "Header.php"; ?>
 <h2>Welcome to GIT</h2>
 <p>Excellence in technology and innovation since (Year).</p>
 <img src="logo.png" alt="School logo" width="150">
 <p>Our mission is to provide quality education in science, technology, and business.</p>
-<?php include "footer.php"; ?>
+<?php include "Footer.php"; ?>
