816accd9be71e6b0e29090c754b0cf64fb6b7e97
RyotaDelaCruz <ryotadelacruz@gmail.com>
2025-10-03 14:45:29 +0800

    Create Header.php

---
+++ User Interface/Header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Greenfield Institute of Technology</title>
+  <link rel="icon" type="image/x-icon" href="logo.png">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+  <h1>Greenfield Institute of Technology</h1>
+  <nav>
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="about.php">About</a></li>
+      <li><a href="courses.php">Courses</a></li>
+      <li><a href="faculty.php">Faculty</a></li>
+      <li><a href="students.php">Students</a></li>
+      <li><a href="contact.php">Contact</a></li>
+    </ul>
+  </nav>
+</header>
+<div class="container">
